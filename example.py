159bc335f02f7e83b4cf0c792c1ce5f2f99f841a
Šarūnas Navickas <sarunas.navickas@e-bros.lt>
2013-09-30 16:01:25 +0300

    A valid example

--- example.py
+++ example.py
@@ -2,4 +2,11 @@ from libs.TfsWrapper import *
 
 tfs = TfsWrapper()
 
-tfs.connect(b"http://tfssrv02.e-bros.fi/", "EbrosMain")
+tfs.connect(b"http://tfsserver.com", "Project")
+
+print(tfs.query("""
+   Select [State], [Title] 
+   From WorkItems
+   Where [Work Item Type] = 'User Story'
+   Order By [State] Asc, [Changed Date] Desc
+"""))
